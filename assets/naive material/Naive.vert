926a9bdaee7522430586ff1877178165ae18ddbd
aegroto <aegroto@protonmail.com>
2018-05-05 00:23:09 +0200

    material update, inputs added

--- assets/naive material/Naive.vert
+++ assets/naive material/Naive.vert
@@ -1,6 +1,7 @@
 uniform mat4 g_WorldViewProjectionMatrix;
 uniform float g_Time;
 uniform float m_TransformSpeed;
+uniform float m_MeshScale;
 
 attribute vec3 inPosition;
 attribute vec2 inTexCoord;
@@ -15,9 +16,14 @@ void main() {
     #ifdef TRANSFORM
         float angle = g_Time * m_TransformSpeed;
 
-        vVertex = vec3(inPosition.x *  cos(angle)   + inPosition.y * sin(angle),
-                       inPosition.x * (-sin(angle)) + inPosition.y * cos(angle),
-                       inPosition.z);
+        vVertex = vec3(inPosition.x * cos(angle) + inPosition.z * (-sin(angle)),
+                       inPosition.y,
+                       inPosition.x * sin(angle) + inPosition.z * cos(angle)
+                    );
+    #endif
+
+    #ifdef SCALING
+        vVertex *= m_MeshScale;
     #endif
 
     gl_Position = g_WorldViewProjectionMatrix * vec4(vVertex, 1.0);
--- src/main/java/com/aegroto/snexample/Main.java
+++ src/main/java/com/aegroto/snexample/Main.java
@@ -1,16 +1,20 @@
 package com.aegroto.snexample;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
-import com.jme3.scene.shape.Sphere;
 import com.jme3.texture.Texture;
-import com.jme3.texture.Texture2D;
 
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication implements AnalogListener {
+    private Material mat;
+    private float meshScale = 1f;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -20,20 +24,44 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        flyCam.setMoveSpeed(100f);
-        Sphere b = new Sphere(150, 150, 2f);
-        // Box b = new Box(1, 1, 1);
+        flyCam.setMoveSpeed(20f);
+        // Sphere b = new Sphere(150, 150, 2f);
+        Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
 
-        Material mat = new Material(assetManager, "naive material/Naive.j3md");
-        Texture texture = assetManager.loadTexture("textures/test texture.png");
+        mat = new Material(assetManager, "naive material/Naive.j3md");
+        Texture texture = assetManager.loadTexture("textures/monkey texture.png");
         texture.setWrap(Texture.WrapMode.Repeat);
         mat.setTexture("Tex", texture);
+
         mat.setFloat("TexScale", .35f);
         mat.setFloat("TransformSpeed", 2f);
+        mat.setFloat("MeshScale", meshScale);
+
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
+        // geom.setLocalRotation(new Quaternion().fromAngles(-FastMath.QUARTER_PI, 0f, 0f));
+
+        inputManager.addMapping("ScaleUp", new KeyTrigger(KeyInput.KEY_U));
+        inputManager.addMapping("ScaleDown", new KeyTrigger(KeyInput.KEY_J));
+        inputManager.addListener(this, "ScaleUp", "ScaleDown");
+    }
+
+    @Override
+    public void onAnalog(String name, float value, float tpf) {
+        switch(name) {
+            case "ScaleUp" :
+                meshScale += .1f;
+                mat.setFloat("MeshScale", meshScale);
+                break;
+            case "ScaleDown" :
+                if(meshScale > 0f) {
+                    meshScale -= .1f;
+                    mat.setFloat("MeshScale", meshScale);
+                }
+                break;
+        }
     }
 
     @Override
